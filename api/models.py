ddaecd01481d5f03365adabebe2c3e2d5725a44f
Anderson Avelllaneda <lamboite10avellaneda@gmail.com>
2025-09-10 15:52:31 -0500

    Creacion del Enpoint Usuarios

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=255, null=False)
+    telefono = models.CharField(max_length=20, null=True, blank=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
