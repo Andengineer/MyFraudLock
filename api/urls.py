c2936b3115e26aa778290f6a5866f5702d85e072
Anderson Avelllaneda <lamboite10avellaneda@gmail.com>
2025-11-12 22:20:46 -0500

    1. antes de este funciona para despliegue 2.3

--- api/urls.py
+++ api/urls.py
@@ -11,17 +11,21 @@ router.register(r'incidentes', IncidenteViewSet)
 router.register(r'configuracion', ConfiguracionViewSet)
 
 urlpatterns = [
-    path('auditoria-api/', AuditoriaView.as_view(), name='auditoria_api'),  # API JSON
-    path('auditoria/', auditoria_view, name='auditoria_front'),  # Frontend HTML
-    path('auditoria-lote/', auditoria_lote_view, name='auditoria_lote'),  # 👈 nuevo
+
+    # ==== FRONTEND PRINCIPAL (HTML) ====
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
+    path('inicio/', inicio_view, name='inicio'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('incidentes-listado/', incidentes_view, name='incidentes_listado'),
     path('incidentes/<int:incidente_id>/detalle/', incidente_detalle_view, name='incidente_detalle'),
+    path('auditoria/', auditoria_view, name='auditoria_front'),
+    path('auditoria-api/', AuditoriaView.as_view(), name='auditoria_api'),
+    path('auditoria-lote/', auditoria_lote_view, name='auditoria_lote'),
     path('configuracion-front/', configuracion_front, name='configuracion_front'),
-    path('inicio/', inicio_view, name='inicio'),
     path('ayuda/', ayuda_view, name='ayuda'),
-    path('login/', login_view, name='login'),  # 👈 NUEVO
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+
+    # ==== API REST ====
     path('', include(router.urls)),
 ]
